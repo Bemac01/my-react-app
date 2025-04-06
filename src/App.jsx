f3d05fd3dac746975bb644117470a9d617e89e95
bemac01 <nwankwochidubem01@gmail.com>
2025-04-06 15:20:24 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Contact from './pages/Contact.jsx'
 import About from './pages/About.jsx'
+import Posts from './pages/Posts.jsx'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
           <Route path='/' element={<Home />}/>
           <Route path='/About' element={<About />}/>
           <Route path='/Contact' element={<Contact />}/>
+          <Route path='/Posts' element={<Posts />}/>
         </Routes>
       </Router>
     </>
---
+++ src/pages/Posts.jsx
@@ -0,0 +1,15 @@
+function Posts() {
+    return (
+        <>
+            <h1>Posts page</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Facere dolorem natus voluptatum, aliquid 
+                voluptate assumenda nam! Tempore, rem animi.
+                Maxime saepe iusto tenetur voluptatibus 
+                possimus incidunt enim. Quod, libero harum.
+            </p>
+        </>
+    )
+}
+
+export default Posts
